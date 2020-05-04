b2b15aa614519d873c6718c11d673a42e572e345
iwmh <hiroshiplusone@gmail.com>
2020-05-04 14:49:40 +0900

    add multi-page navigation(sample)

---
+++ app/src/main/java/com/example/spotifywearapp/FirstScreenFragment.kt
@@ -0,0 +1,35 @@
+package com.example.spotifywearapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+class FirstScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first_screen, container, false)
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_next_screen)
+            .setOnClickListener { v ->
+                Navigation.findNavController(v) // (1)
+                    .navigate(R.id.authScreenFragment) // (2)
+            }
+    }
+
+
+}

a602aa074edd21a0b85fc6fe5496fb7e760f4236
iwmh <hiroshiplusone@gmail.com>
2020-06-02 22:34:10 +0900

    Enabled ambient mode

--- app/src/main/java/com/example/spotifywearapp/Activities/MainActivity.kt
+++ app/src/main/java/com/example/spotifywearapp/Activities/MainActivity.kt
@@ -2,22 +2,46 @@ package com.example.spotifywearapp.Activities
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import androidx.wear.ambient.AmbientModeSupport
 import com.example.spotifywearapp.R
 
-class MainActivity : FragmentActivity() {
+class MainActivity : FragmentActivity(), AmbientModeSupport.AmbientCallbackProvider {
+        /*
+        * Declare an ambient mode controller, which will be used by
+        * the activity to determine if the current mode is ambient.
+        */
+    private lateinit var ambientController: AmbientModeSupport.AmbientController
+
+    override fun getAmbientCallback(): AmbientModeSupport.AmbientCallback = MyAmbientCallback()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        // Enables Always-on
-//        setAmbientEnabled()
+        ambientController = AmbientModeSupport.attach(this)
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
     }
+
+    private class MyAmbientCallback : AmbientModeSupport.AmbientCallback() {
+
+        override fun onEnterAmbient(ambientDetails: Bundle?) {
+            // Handle entering ambient mode
+        }
+
+        override fun onExitAmbient() {
+            // Handle exiting ambient mode
+        }
+
+        override fun onUpdateAmbient() {
+            // Update the content
+        }
+    }
+
 
 
 
